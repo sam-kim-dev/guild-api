ddd0d209c8bde97459bdb6821d1e5c900c667b00
Sam Kim <samuelkimdev@gmail.com>
2021-11-08 22:26:17 -0500

    Get requests specs green
    Most of my specs are being run in the model specs. I've adjusted the
    request specs here to ensure the routes are successful and render the
    proper status codes. I do not believe we need to test out the `recent`
    scope here again since we do that in the model. For extra coverage, we
    definitely can add some robust model specs here, but I'd like to keep
    moving.

--- spec/requests/messages_spec.rb
+++ spec/requests/messages_spec.rb
@@ -1,22 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe "api/v1/messages", type: :request do
+  let(:sender) { User.create(id: 1, name: "Bart Simpson") }
+  let(:recipient) { User.create(id: 2, name: "Homer Simpson") }
+
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    {
+      "data": {
+        "body": "Cowabunga, dude",
+        "recipient_id": recipient.id,
+      }
+    }
   }
+
+  let(:invalid_attributes) {
+    {
+      "data": {
+        "body": "Cowabunga, dude",
+        "recipient_id": nil,
+      }
+    }
+  }
+
+  before do
+    sender.save!
+    recipient.save!
+  end
 
   describe "GET /index" do
     it "renders a successful response" do
-      Message.create! valid_attributes
-      get messages_url, headers: valid_headers, as: :json
+      get api_v1_messages_url, as: :json
       expect(response).to be_successful
     end
   end
 
   describe "GET /show" do
     it "renders a successful response" do
-      message = Message.create! valid_attributes
-      get message_url(message), as: :json
+      get api_v1_sender_messages_url(recipient), as: :json
       expect(response).to be_successful
     end
   end
@@ -25,14 +45,14 @@ RSpec.describe "api/v1/messages", type: :request do
     context "with valid parameters" do
       it "creates a new Message" do
         expect {
-          post messages_url,
-               params: { message: valid_attributes }, headers: valid_headers, as: :json
+          post api_v1_messages_url,
+               params: valid_attributes, as: :json
         }.to change(Message, :count).by(1)
       end
 
       it "renders a JSON response with the new message" do
-        post messages_url,
-             params: { message: valid_attributes }, headers: valid_headers, as: :json
+        post api_v1_messages_url,
+             params: valid_attributes, as: :json
         expect(response).to have_http_status(:created)
         expect(response.content_type).to match(a_string_including("application/json"))
       end
@@ -41,16 +61,16 @@ RSpec.describe "api/v1/messages", type: :request do
     context "with invalid parameters" do
       it "does not create a new Message" do
         expect {
-          post messages_url,
-               params: { message: invalid_attributes }, as: :json
+          post api_v1_messages_url,
+               params: invalid_attributes, as: :json
         }.to change(Message, :count).by(0)
       end
 
       it "renders a JSON response with errors for the new message" do
-        post messages_url,
-             params: { message: invalid_attributes }, headers: valid_headers, as: :json
+        post api_v1_messages_url,
+             params: invalid_attributes, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.content_type).to eq("application/json")
+        expect(response.content_type).to eq("application/json; charset=utf-8")
       end
     end
   end
