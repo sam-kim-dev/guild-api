129ffffc44615a50ee1632fe7f3ee0d6c7d0fce5
Sam Kim <samuelkimdev@gmail.com>
2021-11-08 20:24:53 -0500

    Set up rails associations
    Since the user can be the receiver or sender, I wanted to specify two
    different `has_many`s with each foreign key.
    
    In order to get a full list of messages for a specific user, we need a
    way to query the user's messages by checking both of those foreign key
    fields. We can get back a list of all messages with User#messages.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  has_many :sent_messages,
+           class_name: "Message",
+           foreign_key: :sender_id,
+           dependent: :delete_all
+  has_many :received_messages,
+           class_name: "Message",
+           foreign_key: :recipient_id,
+           dependent: :delete_all
+
+  def messages
+    Message.where('recipient_id = :user_id OR sender_id = :user_id', user_id: self.id)
+  end
 end
