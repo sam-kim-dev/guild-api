5f539c0394f6eb24a7852c14b2257a4c817c7456
Sam Kim <samuelkimdev@gmail.com>
2021-11-08 20:07:18 -0500

    Message creation MVP
    Very basic message creation. I do not think that the sender_id or
    recipient_id should be part of the permitted  params (maybe the
    recipient_id can?). The sender_id should be coming from a
    `before_action` hook that verifies the current_user and sends it
    that way. For now, to get the API working, I will commit this.

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -9,7 +9,7 @@ class Api::V1::MessagesController < ApplicationController
     @message = Message.new(message_params)
 
     if @message.save
-      render json: @message, status: :created, location: @message
+      render json: @message, status: :created
     else
       render json: @message.errors, status: :unprocessable_entity
     end
@@ -22,6 +22,7 @@ private
   end
 
   def message_params
-    params.fetch(:message, {})
+    params.require(:data)
+          .permit(:body, :sender_id, :recipient_id)
   end
 end
