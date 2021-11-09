1bbcc4f3fc85f55e8a9413150539c0bc5585c680
Sam Kim <samuelkimdev@gmail.com>
2021-11-08 22:46:51 -0500

    Seed some users
    We "authenticate" using the first User in this API. I wanted to seed
    some lorem ipsum messages, but feel low on time.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.find_or_initialize_by(id: 1) { |user|
+  user.name = "Homer Simpson"
+}.save!
+
+User.find_or_initialize_by(id: 2) { |user|
+  user.name = "Marge Simpson"
+}.save!
+
+User.find_or_initialize_by(id: 3) { |user|
+  user.name = "Bart Simpson"
+}.save!
+
+User.find_or_initialize_by(id: 4) { |user|
+  user.name = "Lisa Simpson"
+}.save!
+
+User.find_or_initialize_by(id: 5) { |user|
+  user.name = "Maggie Simpson"
+}.save!
