b19ca713a872b11c4d61cc504863b96cb8f9a442
Sam Kim <samuelkimdev@gmail.com>
2021-11-08 20:36:49 -0500

    Add `recent` scope to Message
    When we query for "recent" messages, we want to only look back 30 days.
    We may need to change the definition of what "recent" means so I've
    added some safeguards for that. Recent will max out at 100 messages.
    
    1 - We can update an ENV to change the recency app-wide without any code
    changes.
    2 - We can update the class_attribute, `recency_threshold_days`, for a
    specific use case (or for a spec).

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,11 @@ class Message < ApplicationRecord
              class_name: "User"
   belongs_to :sender,
              class_name: "User"
+
+  class_attribute :recency_threshold_days,
+                  default: Integer(ENV.fetch("MESSAGE_RECENCY_THRESHOLD_DAYS", 30))
+
+  scope :recent, -> {
+    where('created_at > ?', recency_threshold_days.days.ago).limit(100)
+  }
 end
